32a9f1c52ed637b0f0c38757a7c672256d57b8ac
aedilyan <artur-edilyan@live.com>
2022-02-02 13:03:00 +0400

    prod fix

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const express = require("express"),
 
 
 // prod:Have Node serve the files for our built React app
-//app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // middleware for bodyparser
 app.use(bodyParser.json());
